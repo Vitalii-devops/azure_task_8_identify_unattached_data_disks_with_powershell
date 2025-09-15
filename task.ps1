c3ff5dea2836bda1bcd52ea249f067975931ab08
Vitalii <vitalyoirshko@gmail.com>
2025-09-15 20:01:23 +0300

    solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,8 @@
 # Write your code here
+
+$resourceGroupName = "mate-azure-task-5"
+$allDisks = Get-AzDisk -ResourceGroupName $resourceGroupName
+
+$unattachedDisks = $allDisks | Where-Object { -not $_.ManagedBy }
+
+$unattachedDisks | ConvertTo-Json -Depth 5 | Out-File -FilePath "result.json" -Encoding utf8
